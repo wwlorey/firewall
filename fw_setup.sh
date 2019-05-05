91d5d125467dc68f9f0b8fb7d7dfa5358a0cd7c2
William Lorey <wwlytc@mst.edu>
2019-05-05 12:57:45 -0500

    Add SSH

--- fw_setup.sh
+++ fw_setup.sh
@@ -12,6 +12,9 @@ sudo /sbin/iptables -A INPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT
 # Set port 443 AS open for TCP traffic
 sudo /sbin/iptables -A INPUT -m state --state NEW -p tcp --dport 443 -j ACCEPT
 
+# Setup SSH
+sudo /sbin/iptables -A INPUT -p tcp -s 12.34.56.78 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+
 # Save the rules we just created
 # iptables-save dumps the iptables config to the screen, so redirect it to the rules file
 sudo bash -c "/sbin/iptables-save > /etc/iptables.rules"
