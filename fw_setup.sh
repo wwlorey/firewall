50be0206b2c4d028cbec9f91ea883f757083a8ae
William Lorey <wwlorey@gmail.com>
2019-05-05 09:45:06 -0500

    Non-working 80 & 443

--- fw_setup.sh
+++ fw_setup.sh
@@ -4,3 +4,12 @@
 # including comments describing in detail what 
 # each componend of each command on every line does.
 
+# Set port 80 as open for TCP traffic
+/sbin/iptables -A INPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT
+
+# Set port 443 AS open for TCP traffic
+/sbin/iptables -A INPUT -m state --state NEW -p tcp --dport 443 -j ACCEPT
+
+# Save the rules we just created
+iptables-save
+
