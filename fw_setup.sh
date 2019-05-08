1f1ceccef481203afb5fa12ee503f19a2194756b
William Lorey <wwlytc@mst.edu>
2019-05-08 10:15:00 -0500

    Only allow outgoing connections to debian update server

--- fw_setup.sh
+++ fw_setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 # Write your IPtables configuration script here, 
 # including comments describing in detail what 
 # each componend of each command on every line does.
@@ -45,8 +46,6 @@ sudo /sbin/iptables -P OUTPUT DROP
 #         (INPUT, FORWARD, OUTPUT)
 #############################################
 
-# sudo iptables -A  -i <interface> -p <protocol (tcp/udp) > -s <source> --dport <port no.>  -j <target>
-
 #######################
 # INPUT chain
 #######################
@@ -75,7 +74,6 @@ sudo /sbin/iptables -A OUTPUT -p tcp --sport 80 -m conntrack --ctstate ESTABLISH
 # Catch-all to reject anything not matching the above rules
 sudo /sbin/iptables -A INPUT -j REJECT
 
-
 #######################
 # FORWARD CHAIN
 #######################
@@ -84,7 +82,6 @@ sudo /sbin/iptables -A INPUT -j REJECT
 # Reject all forwards
 sudo /sbin/iptables -A FORWARD -j REJECT
 
-
 #######################
 # OUTPUT CHAIN
 #######################
@@ -92,11 +89,11 @@ sudo /sbin/iptables -A FORWARD -j REJECT
 # Accept loopback output
 sudo /sbin/iptables -A OUTPUT -o lo -j ACCEPT
 
-# HTTPS port allowed out
-sudo /sbin/iptables -A OUTPUT -p tcp --dport https -j ACCEPT
-
-# HTTP port allowed out
-sudo /sbin/iptables -A OUTPUT -p tcp --dport http -j ACCEPT
+# Allow outgoing connections to debian update server
+# host -t a www.debian.org produced IPs 149.20.4.15 and 128.31.0.62
+# -d <IP address> means destination
+sudo /sbin/iptables -A OUTPUT -d 149.20.4.15 -j ACCEPT
+sudo /sbin/iptables -A OUTPUT -d 128.31.0.62 -j ACCEPT
 
 # DNS allowed out on 53
 sudo /sbin/iptables -A OUTPUT -p udp --dport domain -j ACCEPT
